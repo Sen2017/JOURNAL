e09e9eee132d743d447246738dee5e6b7d07c861
Sen2017 <casintahan47at@gmail.com>
2017-03-15 09:11:34 -1000

    json start

---
+++ app.js
@@ -0,0 +1,39 @@
+ /*var jsonDataBody =  [
+	{
+		"header": "center"		
+	},
+	{
+		"div": {
+			"class": "aside",
+			[
+			"img", 
+			"h6", 
+			"nav"
+			]
+		} 
+	}
+
+];	*/
+
+var simplifiedJSON = [
+	{
+		"journalEntry": "1",
+		"date": "August 15, 2016",
+		"title": "Purpose",
+		"content": ["I took this class because I find the subject of coding and programming interesting. Also because it looks COOL.", "From this class I want to see the world behind the apps I see everyday."]
+		"img": undefined
+	},
+	{
+		"journalEntry": "2",
+		"date": "August 24, 2016",
+		"title": "Difference between UX and UI",
+		"content": ["UX is defined as User Experience. It includes user satisfaction and hassle-free service.", "UI is the user interface which covers functionality. It mainly includes ituitivity and ease of access."]
+	},
+	{
+		"journalEntry": "3",
+		"date": "September 2, 2016",
+		"title": "HTML vs CSS",
+		"content": ""
+	}
+
+]
